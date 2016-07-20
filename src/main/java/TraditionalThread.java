ed7d88f24d51be4be550e853780c0b0635ec4803
Mark Tao <t22w@qq.com>
2016-07-20 20:19:59 +0800

    Initial commit

---
+++ src/main/java/TraditionalThread.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Mark on 2016/7/20.
+ */
+public class TraditionalThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+        thread.start();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread1.start();
+    }
+}
